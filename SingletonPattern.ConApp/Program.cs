c38f6864bbc3a25cd69676823a6ae0310096d0e5
ggehrer <g.gehrer@hotmail.com>
2024-12-15 08:55:25 +0100

    Implemented con app.

--- SingletonPattern.ConApp/Program.cs
+++ SingletonPattern.ConApp/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SingletonPattern.ConApp
+{
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main method that runs the application.
+        /// </summary>
+        static void Main()
+        {
+            Console.WriteLine("Demo Singleton-Pattern!");
+
+            RunWithEagerSingleton();
+            RunWithLazySingleton();
+            RunWithLockSingleton();
+            RunWithStaticSingleton();
+        }
+
+        /// <summary>
+        /// Demonstrates the use of the eager singleton pattern.
+        /// </summary>
+        static void RunWithEagerSingleton()
+        {
+            var singleton1 = Logic.WithEager.Singleton.Instance;
+            var singleton2 = Logic.WithEager.Singleton.Instance;
+
+            singleton1.DoSomething();
+            Console.WriteLine($"Instances are equals: {singleton1 == singleton2}");
+        }
+
+        /// <summary>
+        /// Demonstrates the use of the lazy singleton pattern.
+        /// </summary>
+        static void RunWithLazySingleton()
+        {
+            var singleton1 = Logic.WithLazy.Singleton.Instance;
+            var singleton2 = Logic.WithLazy.Singleton.Instance;
+
+            singleton1.DoSomething();
+            Console.WriteLine($"Instances are equals: {singleton1 == singleton2}");
+        }
+
+        /// <summary>
+        /// Demonstrates the use of the singleton pattern with locking.
+        /// </summary>
+        static void RunWithLockSingleton()
+        {
+            var singleton1 = Logic.WithLock.Singleton.Instance;
+            var singleton2 = Logic.WithLock.Singleton.Instance;
+
+            singleton1.DoSomething();
+            Console.WriteLine($"Instances are equals: {singleton1 == singleton2}");
+        }
+
+        /// <summary>
+        /// Demonstrates the use of the static singleton pattern.
+        /// </summary>
+        static void RunWithStaticSingleton()
+        {
+            Logic.WithStatic.Singleton.DoSomething();
+        }
+    }
+}
